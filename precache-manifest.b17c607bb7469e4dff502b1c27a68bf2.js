406780bf5553876b83d917f7dca6d0edf6b95405
Michal Hazucha <hazuchamichal@gmail.com>
2021-03-14 19:40:07 +0100

    Updates

---
+++ precache-manifest.b17c607bb7469e4dff502b1c27a68bf2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "881773577b6adb718d47afe724b93455",
+    "url": "/covid19WI/index.html"
+  },
+  {
+    "revision": "c4294bc661a45511b84b",
+    "url": "/covid19WI/static/css/main.4aca4c92.chunk.css"
+  },
+  {
+    "revision": "395edaf3a0e523d0f3de",
+    "url": "/covid19WI/static/js/2.846ad8bc.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/covid19WI/static/js/2.846ad8bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4294bc661a45511b84b",
+    "url": "/covid19WI/static/js/main.c7af235c.chunk.js"
+  },
+  {
+    "revision": "95a00a673b397248b63a",
+    "url": "/covid19WI/static/js/runtime-main.bb5c7888.js"
+  },
+  {
+    "revision": "026e38abdbe6fb44763a279e08feb0d0",
+    "url": "/covid19WI/static/media/19.026e38ab.png"
+  },
+  {
+    "revision": "9556b8b476a15f7390f86bc9116e4eda",
+    "url": "/covid19WI/static/media/r-o-r.9556b8b4.png"
+  }
+]);
